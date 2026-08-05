4adcb170501ec972a85a9669d8122a14507bc36b
BeauZanni <stellarlaugh@gmail.com>
2026-08-05 15:24:04 -0400

    algo

--- .github/workflows/ejecutador.js
+++ .github/workflows/ejecutador.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 const path = require('path');
-const carpeta = path.join(process.cwd(), '../../fichas');
-const archivoIndice = path.join(process.cwd(), '../../registro.json');
 
-function actualizarIndice() {
+const carpeta = path.join(process.cwd(), 'fichas');
+const archivoRegistros = path.join(process.cwd(), 'registros.json');
+
+function actualizarRegistros() {
   const archivos = fs.readdirSync(carpeta);
-  const nombres = [...new Set(
-    archivos.map(f => path.parse(f).name)
-  )];
-
-  fs.writeFileSync(archivoIndice, JSON.stringify(nombres, null, 2));
-  console.log('✅ registro.json actualizado con', nombres.length, 'entradas');
+  const nombres = [...new Set(archivos.map(f => path.parse(f).name))];
+  fs.writeFileSync(archivoRegistros, JSON.stringify(nombres, null, 2));
+  console.log('✅ registros.json actualizado con', nombres.length, 'entradas');
 }
 
-actualizarIndice();
+actualizarRegistros();
+
